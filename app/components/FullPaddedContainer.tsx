d65688075d5e7c0e79bac8f25a139dab13ff799e
Jack Hansjee <jack.hansjee@me.com>
2025-04-24 16:56:48 +0200

    Add parameters to paddedcontainer to make it more versatile

--- app/components/FullPaddedContainer.tsx
+++ app/components/FullPaddedContainer.tsx
@@ -1,14 +1,22 @@
 interface FullPaddedContainerProps {
   children: React.ReactNode;
   flexGap?: `gap-${number}`;
+  padding?: "normal" | "thick";
+  fullPage?: boolean;
 }
 
 const FullPaddedContainer = ({
   children,
   flexGap = "gap-5",
+  padding = "normal",
+  fullPage = false,
 }: FullPaddedContainerProps) => {
   return (
-    <div className={`w-full p-5 mt-20 absolute flex flex-col ${flexGap}`}>
+    <div
+      className={`w-full ${padding === "normal" ? "p-5" : "p-10"} ${
+        fullPage && "h-[calc(100vh_-_5rem)]"
+      } relative flex flex-col items-center justify-center box-border ${flexGap}`}
+    >
       {children}
     </div>
   );
