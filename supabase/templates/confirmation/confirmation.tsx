9f0c1ac4fc8c489deea570c682e08dfa5ecf5ebe
Jack Hansjee <jack.hansjee@me.com>
2025-04-24 15:49:02 +0200

    Change width and add shadows to email confirmation template

--- supabase/templates/confirmation/confirmation.tsx
+++ supabase/templates/confirmation/confirmation.tsx
@@ -8,18 +8,18 @@ const Confirmation = ({}: ConfirmationProps) => {
   return (
     <EmailTemplate>
       <main className="flex items-center justify-center w-screen h-screen">
-        <div className="flex flex-col gap-28 w-96 bg-secondary p-8 rounded-2xl shadow-xl">
+        <div className="flex flex-col gap-28 w-80 bg-secondary p-8 rounded-2xl shadow-xl">
           <Img src="../../../app/svgs/Logo.svg" width="100%" />
           <div className="flex flex-col gap-5">
             <p>Please click the button below to confirm your email address.</p>
             <a
               href="{{ .SiteURL }}/auth/confirm?token_hash={{ .TokenHash }}&type=email&next={{ .RedirectTo }}"
-              className="w-full leading-none font-bold transition-all py-3 rounded-lg bg-primary text-background no-underline flex items-center justify-center"
+              className="w-full leading-none font-bold transition-all py-3 rounded-lg bg-primary text-background no-underline flex items-center justify-center shadow-lg"
               target="_blank"
             >
               <span className="w-min text-nowrap">Confirm Email</span>
             </a>
-            <p className="text-text-dim">
+            <p className="text-text-dim text-sm">
               If you did not recently sign up for a Campus Collective account,
               please ignore this email.
             </p>
