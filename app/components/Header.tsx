a34860bfcd17fc27c4e6f28fc5a07f5048049230
Jack Hansjee <jack.hansjee@me.com>
2025-05-01 17:37:39 +0100

    Add conditional logic to the rendering of the log in/avatar button in the header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,17 +3,50 @@ import Logo from "../svgs/Logo.svg?react";
 import User from "../svgs/User.svg?react";
 import LogIn from "../svgs/LogIn.svg?react";
 import SquareButton from "./SquareButton";
+import { Link } from "react-router";
 
-const Header = () => {
+interface HeaderProps {
+  authenticated: boolean;
+  avatar_url?: string;
+}
+
+const Header = ({ authenticated, avatar_url }: HeaderProps) => {
+  const authButtons = {
+    logIn: (
+      <SquareButton type="primary" isLink={true} path="/log-in">
+        <LogIn stroke="#f7f4e9" />
+      </SquareButton>
+    ),
+    noAvatar: (
+      <SquareButton type="primary" isLink={true} path="/account">
+        <User stroke="#f7f4e9" />
+      </SquareButton>
+    ),
+    avatar: (
+      <Link to="/account">
+        <img src={avatar_url} />
+      </Link>
+    ),
+  };
+  let option: "logIn" | "noAvatar" | "avatar";
+
+  if (!authenticated) {
+    option = "logIn";
+  } else if (avatar_url) {
+    option = "avatar";
+  } else {
+    option = "noAvatar";
+  }
+
   return (
     <header className="w-full h-20 px-5 bg-background-light shadow-below flex items-center justify-between fixed z-10">
       <SquareButton type="primary">
         <Menu stroke="#f7f4e9" />
       </SquareButton>
-      <Logo />
-      <SquareButton type="primary" isLink={true} path="/log-in">
-        <LogIn stroke="#f7f4e9" />
-      </SquareButton>
+      <Link to="/">
+        <Logo />
+      </Link>
+      {authButtons[option]}
     </header>
   );
 };
