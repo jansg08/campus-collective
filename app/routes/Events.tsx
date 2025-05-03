2dd4b0506763d7ba1768ed87102b92c7893f0cb8
Jack Hansjee <jack.hansjee@me.com>
2025-05-03 23:26:48 +0100

    Create grid layout for event cards and add university dropdown to toggle between different universities websites

--- app/routes/Events.tsx
+++ app/routes/Events.tsx
@@ -1,23 +1,38 @@
 import { eq } from "drizzle-orm";
-import type { FC } from "react";
-import { data, Form } from "react-router";
+import { useRef, useState } from "react";
+import { data, Form, NavLink } from "react-router";
 import { db } from "src/db";
 import { categoriesTable } from "src/db/schema/categories";
 import { eventsTable } from "src/db/schema/events";
 import { venuesTable } from "src/db/schema/venues";
 import EventCard from "~/components/EventCard";
-import FullPaddedContainer from "~/components/FullPaddedContainer";
 import InputWithIcon from "~/components/InputWithIcon";
 import SquareButton from "~/components/SquareButton";
 import ChevronSmall from "~/svgs/ChevronSmall.svg?react";
 import Filter from "~/svgs/Filter.svg?react";
 import Search from "~/svgs/Search.svg?react";
+import Tick from "~/svgs/Tick.svg?react";
+import { Squircle } from "ldrs/react";
+import "ldrs/react/Squircle.css";
 import type { Route } from "./+types/Events";
 import { universitiesTable } from "src/db/schema/universities";
+import useOutsideClick from "~/utils/useOutSideClick";
+
+interface University {
+  id: number;
+  name: string;
+  crestUrl: string;
+  slug: string;
+}
+
+interface UniversitiesDropdownProps {
+  universities: University[];
+  selected: number;
+}
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
   const { universitySlug } = params;
-  const results = await db
+  const eventsQuery = db
     .select({
       id: eventsTable.id,
       eventPhotoUrl: eventsTable.coverPhotoUrl,
@@ -27,8 +42,7 @@ export const loader = async ({ params }: Route.LoaderArgs) => {
       endTime: eventsTable.endTime,
       venue: venuesTable.name,
       defaultPhotoUrl: categoriesTable.coverPhotoUrl,
-      uniName: universitiesTable.name,
-      uniCrest: universitiesTable.crestUrl,
+      uniId: universitiesTable.id,
     })
     .from(eventsTable)
     .innerJoin(venuesTable, eq(eventsTable.venueId, venuesTable.id))
@@ -39,7 +53,19 @@ export const loader = async ({ params }: Route.LoaderArgs) => {
     )
     .where(eq(universitiesTable.slug, universitySlug));
 
-  const events = results.map(
+  const uniQuery = db
+    .select({
+      id: universitiesTable.id,
+      name: universitiesTable.name,
+      crestUrl: universitiesTable.crestUrl,
+      slug: universitiesTable.slug,
+    })
+    .from(universitiesTable)
+    .orderBy(universitiesTable.slug);
+
+  const [eventResults, uniResults] = await Promise.all([eventsQuery, uniQuery]);
+
+  const events = eventResults.map(
     ({ eventPhotoUrl, defaultPhotoUrl, price, ...rest }) => ({
       photoUrl: eventPhotoUrl ?? defaultPhotoUrl,
       price: price / 100,
@@ -47,33 +73,94 @@ export const loader = async ({ params }: Route.LoaderArgs) => {
     })
   );
 
+  const selectedUni = uniResults.findIndex(
+    ({ slug }) => slug === universitySlug
+  );
+
+  const universities = uniResults.map(({ crestUrl, ...rest }) => ({
+    crestUrl: `http://127.0.0.1:54321/storage/v1/object/public${crestUrl}`,
+    ...rest,
+  }));
+
   return data({
     events,
-    university: {
-      name: results[0].uniName,
-      crestUrl: `http://127.0.0.1:54321/storage/v1/object/public${results[0].uniCrest}`,
-    },
+    universities,
+    selectedUni,
   });
 };
 
-const Events = ({ loaderData }: Route.ComponentProps) => {
-  const {
-    events,
-    university: { name, crestUrl },
-  } = loaderData;
+const UniversitiesDropdown = ({
+  universities,
+  selected,
+}: UniversitiesDropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { crestUrl: currentUniCrest, name: currentUniName } =
+    universities[selected];
+
+  const dropdownRef = useRef<HTMLUListElement>(null!);
+  useOutsideClick({
+    ref: dropdownRef,
+    handler: () => setIsOpen(false),
+  });
   return (
-    <FullPaddedContainer>
-      <div className="w-full">
-        <p className="text-sm text-text-dim">Viewing events for</p>
-        <div className="flex items-center gap-1">
-          <img src={crestUrl} className="size-6 inline rounded-full" />
-          <button className="flex items-center gap-1.5 text-2xl rounded-sm hover:bg-gray-200 px-1 transition-all">
-            <span className="text-start">{name}</span>
-            <ChevronSmall stroke="#044c3b" />
-          </button>
-        </div>
+    <div
+      className={`flex items-center gap-1 relative w-fit transition-all ml-4 px-1 ${
+        isOpen ? "bg-gray-200" : "hover:bg-gray-200"
+      } rounded-sm`}
+    >
+      <img src={currentUniCrest} className="size-6 inline rounded-full" />
+      <button
+        className="flex items-center gap-1.5 text-2xl  px-1"
+        onClick={() => setIsOpen(true)}
+      >
+        <span className="text-start">{currentUniName}</span>
+        <ChevronSmall stroke="#044c3b" />
+      </button>
+      {isOpen && (
+        <ul
+          className="absolute z-10 top-[calc(100%+var(--spacing))] left-0 max-h-[40vh] box-border w-fit overflow-y-auto bg-background-light py-1.5 rounded-lg flex flex-col shadow-lg"
+          ref={dropdownRef}
+        >
+          {universities.map(({ name, crestUrl, slug, id }) => (
+            <li key={id}>
+              <NavLink
+                to={`/${slug}/events`}
+                className="flex items-center gap-1.5 py-1.5 px-3 hover hover:bg-[#E9E6DC] transition-all"
+              >
+                {({ isActive, isPending }) => (
+                  <>
+                    <img
+                      src={crestUrl}
+                      className="size-6 inline rounded-full"
+                    />
+                    <span className="leading-normal min-w-32">{name}</span>
+                    {isActive && <Tick stroke="#4BA590" />}
+                    {isPending && (
+                      <Squircle color="#4BA590" size={12} stroke={2} />
+                    )}
+                  </>
+                )}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+const Events = ({ loaderData }: Route.ComponentProps) => {
+  const { events, universities, selectedUni } = loaderData;
+  return (
+    <div className="flex flex-col">
+      <div className="w-full pt-5">
+        <p className="text-sm text-text-dim px-5">Viewing events for</p>
+        <UniversitiesDropdown
+          universities={universities}
+          selected={selectedUni}
+        />
       </div>
-      <div className="flex gap-3 w-full">
+      <div className="flex gap-3 w-full sticky top-20 p-5 blur-down">
         <SquareButton colour="secondary">
           <Filter stroke="#044c3b" />
         </SquareButton>
@@ -86,10 +173,14 @@ const Events = ({ loaderData }: Route.ComponentProps) => {
           />
         </Form>
       </div>
-      {events.map(({ id, ...rest }) => (
-        <EventCard {...rest} key={id} />
-      ))}
-    </FullPaddedContainer>
+      <ul className="grid grid-cols-(--event-card-cols) gap-5 px-5">
+        {events.map(({ id, ...rest }) => (
+          <li key={id}>
+            <EventCard {...rest} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
