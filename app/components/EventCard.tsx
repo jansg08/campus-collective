8f723bad17c85fdb12b422e10b4b8c779aad5d5b
Jack Hansjee <jack.hansjee@me.com>
2025-05-05 18:15:01 +0100

    Convert each event card into a link that points to the event it describes

--- app/components/EventCard.tsx
+++ app/components/EventCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import EventDetails from "./EventDetails";
 import WideButton from "./WideButton";
 import formatPrice from "~/utils/formatPrice";
@@ -9,6 +10,8 @@ interface EventCardProps {
   startTime: Date;
   endTime: Date;
   venue: string;
+  uniSlug: string;
+  id: number;
 }
 
 const EventCard = ({
@@ -18,34 +21,38 @@ const EventCard = ({
   startTime,
   endTime,
   venue,
+  uniSlug,
+  id,
 }: EventCardProps) => {
   return (
-    <div className="w-full rounded-lg shadow-lg">
-      <div
-        style={{ backgroundImage: `url(${photoUrl})` }}
-        className="aspect-video flex justify-between gap-4 items-start py-4 pr-4 rounded-t-lg bg-cover bg-center"
-      >
-        <div className="h-full flex flex-col justify-end">
-          <h3 className="text-background bg-[rgba(4,76,59,0.8)] leading-none font-bold p-4 max-w-80">
-            {title}
-          </h3>
+    <Link to={`/${uniSlug}/events/${id}`}>
+      <div className="w-full rounded-lg shadow-lg">
+        <div
+          style={{ backgroundImage: `url(${photoUrl})` }}
+          className="aspect-video flex justify-between gap-4 items-start py-4 pr-4 rounded-t-lg bg-cover bg-center"
+        >
+          <div className="h-full flex flex-col justify-end">
+            <h3 className="text-background bg-[rgba(4,76,59,0.8)] leading-none font-bold p-4 max-w-80">
+              {title}
+            </h3>
+          </div>
+          <span className="text-sm text-background bg-accent px-2 py-0.5 font-bold leading-none rounded-xs">
+            {formatPrice(price)}
+          </span>
         </div>
-        <span className="text-sm text-background bg-accent px-2 py-0.5 font-bold leading-none rounded-xs">
-          {formatPrice(price)}
-        </span>
+        <div className="p-4 bg-secondary-light flex flex-col gap-2 rounded-b-lg">
+          <EventDetails
+            variant="small"
+            startTime={startTime}
+            endTime={endTime}
+            venue={venue}
+          />
+          <WideButton colour="primary" isThin>
+            View Details
+          </WideButton>
+        </div>
       </div>
-      <div className="p-4 bg-secondary-light flex flex-col gap-2 rounded-b-lg">
-        <EventDetails
-          variant="small"
-          startTime={startTime}
-          endTime={endTime}
-          venue={venue}
-        />
-        <WideButton colour="primary" isThin>
-          View Details
-        </WideButton>
-      </div>
-    </div>
+    </Link>
   );
 };
 
