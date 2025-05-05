e10955ba31b50452d87e64c1fbe31d21252a75d8
Jack Hansjee <jack.hansjee@me.com>
2025-05-05 16:36:50 +0100

    Create reusable map container using react-leaflet

---
+++ app/components/LeafletMap.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { useEffect } from "react";
+
+export interface LeafletMapProps {
+  center: [number, number];
+  markerPosition?: [number, number];
+  zoom?: number;
+  zoomControl?: boolean;
+  variant?: "" | "Accent" | "Inverted" | "AccentInverted";
+  popUpContent?: string | React.ReactNode;
+  borderRadius?: `${string}px`;
+  expanded?: boolean;
+}
+
+const createDynamicMarker = (variant: string) =>
+  new L.Icon({
+    iconUrl: `/MapMarker${variant}.png`,
+    iconRetinaUrl: `/MapMarker${variant}Retina.png`,
+    iconSize: [55, 65],
+    iconAnchor: [27.5, 70],
+    popupAnchor: [0, -55],
+  });
+
+const ChangeLocation = ({
+  location,
+  expanded,
+}: {
+  location: [number, number];
+  expanded: boolean;
+}) => {
+  const map = useMap();
+  useEffect(() => {
+    if (expanded) {
+      map.invalidateSize(true);
+    }
+  }, [expanded, map]);
+
+  return null;
+};
+
+export const LeafletMap = ({
+  center,
+  markerPosition = center,
+  zoom = 16,
+  zoomControl = true,
+  variant = "",
+  popUpContent,
+  borderRadius = "0px",
+  expanded = true,
+}: LeafletMapProps) => {
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      zoomControl={expanded && zoomControl}
+      style={{
+        height: "100%",
+        width: "100%",
+        borderRadius,
+      }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      <Marker position={markerPosition} icon={createDynamicMarker(variant)}>
+        {popUpContent && <Popup>{popUpContent}</Popup>}
+      </Marker>
+      <ChangeLocation location={center} expanded={expanded} />
+    </MapContainer>
+  );
+};
