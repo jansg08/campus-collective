5a37250ec0c3e931a621f1444bf2965e612364a2
Jack Hansjee <jack@hansjee.com>
2025-05-06 14:34:47 +0100

    Move sign up confirmation for into it's own component rendered through an outlet in confirm booking

--- app/routes/ConfirmBooking.tsx
+++ app/routes/ConfirmBooking.tsx
@@ -1,60 +1,25 @@
-import {
-  data,
-  Form,
-  redirect,
-  useNavigate,
-  useOutletContext,
-} from "react-router";
+import { Outlet, redirect, useNavigate, useOutletContext } from "react-router";
 import { useLocation } from "react-router";
 import Modal from "~/components/Modal";
 import type { Route } from "./+types/ConfirmBooking";
-import { format } from "date-fns";
-import WideButton from "~/components/WideButton";
 import { getSupabaseClient } from "~/auth/supabase.server";
-import { db } from "src/db";
-import { bookingsTable } from "src/db/schema/bookings";
 
-interface OutletContext {
+export interface EventOutletContext {
   event: {
-    venueName: string;
     title: string;
+    description: string | null;
+    venue: {
+      name: string;
+      coords: {
+        x: number;
+        y: number;
+      } | null;
+    } | null;
     startTime: Date;
     endTime: Date;
     price: number;
   };
 }
-
-export const action = async ({ request }: Route.ActionArgs) => {
-  const eventId = Number.parseInt(
-    request.url.match(/(?<=\/)\d+(?=\/confirm)/gi)?.join() || ""
-  );
-  const { supabase, headers } = getSupabaseClient(request);
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser();
-
-  if (user && eventId) {
-    const [newBooking] = await db
-      .insert(bookingsTable)
-      .values({ userId: user.id, eventId: eventId })
-      .returning({
-        dateBooked: bookingsTable.dateBooked,
-        paymentRef: bookingsTable.paymentRef,
-      })
-      .onConflictDoNothing();
-
-    if (newBooking) {
-      return redirect(`${request.url}/success`, { headers });
-    }
-  }
-
-  if (user) {
-    return data({ err: "Selected event does not exist" }, { headers });
-  }
-
-  return redirect("/log-in", { headers });
-};
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
   const { supabase, headers } = getSupabaseClient(request);
@@ -69,30 +34,13 @@ export const loader = async ({ request }: Route.LoaderArgs) => {
 };
 
 const ConfirmBooking = ({}: Route.ComponentProps) => {
-  const { event } = useOutletContext<OutletContext>();
+  const { event } = useOutletContext<EventOutletContext>();
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
   return (
-    <Modal closeModal={() => navigate(pathname.replace("/confirm", ""))}>
-      <h3>Confirm Booking</h3>
-      <div className="leading-normal text-sm">
-        <p>
-          {event.title} @ {event.venueName}
-        </p>
-        <p>
-          {format(event.startTime, "iiii do MMMM y")},{" "}
-          {format(event.startTime, "HH:mm")}-{format(event.endTime, "HH:mm")}
-        </p>
-      </div>
-      {event.price === 0 && (
-        <Form method="post" action={pathname} className="flex flex-col gap-2">
-          <WideButton type="submit">Free • Confirm Booking</WideButton>
-          <p className="text-2xs text-text-dim">
-            This event is free - no payment will be taken
-          </p>
-        </Form>
-      )}
+    <Modal closeModal={() => navigate(pathname.replace(/\/confirm.*/, ""))}>
+      <Outlet context={{ event }} />
     </Modal>
   );
 };
---
+++ app/routes/ConfirmBookingForm.tsx
@@ -0,0 +1,89 @@
+import { data, Form, redirect, useOutletContext } from "react-router";
+import type { EventOutletContext } from "~/routes/ConfirmBooking";
+import WideButton from "../components/WideButton";
+import { format } from "date-fns";
+import { getSupabaseClient } from "~/auth/supabase.server";
+import { bookingsTable } from "src/db/schema/bookings";
+import type { Route } from "./+types/ConfirmBookingForm";
+import { db } from "src/db";
+
+interface ConfirmBookingFormProps {}
+
+export const action = async ({ request, params }: Route.ActionArgs) => {
+  const eventId = Number(params.eventId);
+  const { supabase, headers } = getSupabaseClient(request);
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (user && eventId) {
+    const [newBooking] = await db
+      .insert(bookingsTable)
+      .values({ userId: user.id, eventId: eventId })
+      .returning({
+        dateBooked: bookingsTable.dateBooked,
+        paymentRef: bookingsTable.paymentRef,
+      })
+      .onConflictDoNothing();
+
+    if (newBooking) {
+      return redirect(`${request.url}/success`, { headers });
+    }
+    return data(
+      {
+        error: {
+          code: "booking_exists",
+          msg: "The booking already exists for the given event and user id",
+        },
+      },
+      { headers }
+    );
+  }
+
+  if (user) {
+    return data(
+      {
+        error: {
+          code: "event_not_found",
+          msg: "The event with the given event id does not exist",
+        },
+      },
+      { headers }
+    );
+  }
+
+  return redirect("/log-in", { headers });
+};
+
+const ConfirmBookingForm = ({ actionData }: Route.ComponentProps) => {
+  const error = actionData?.error;
+  console.log(error);
+  const { event } = useOutletContext<EventOutletContext>();
+  return (
+    <>
+      <h3>Confirm Booking</h3>
+      <div className="leading-normal text-sm flex flex-col gap-2">
+        <h6 className="font-bold">What?</h6>
+        <p>{event.title}</p>
+        <h6 className="font-bold">Where?</h6>
+        <p>{event.venue?.name}</p>
+        <h6 className="font-bold">When?</h6>
+        <p>
+          {format(event.startTime, "iiii do MMMM y")},{" "}
+          {format(event.startTime, "HH:mm")}-{format(event.endTime, "HH:mm")}
+        </p>
+      </div>
+      {event.price === 0 && (
+        <Form method="post" className="flex flex-col gap-2">
+          <WideButton type="submit">Free • Confirm Booking</WideButton>
+          <p className="text-2xs text-text-dim">
+            This event is free - no payment will be taken
+          </p>
+        </Form>
+      )}
+    </>
+  );
+};
+
+export default ConfirmBookingForm;
