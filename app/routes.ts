5a30f858b416d2b86f1b2ce0040172cc0ab2c726
Jack Hansjee <jack@hansjee.com>
2025-05-06 14:36:06 +0100

    Add new components as the routes /confirm and /confirm/success respectively

--- app/routes.ts
+++ app/routes.ts
@@ -15,7 +15,10 @@ export default [
   ]),
   route(":universitySlug/events", "routes/Events.tsx"),
   route(":universitySlug/events/:eventId", "routes/EventListing.tsx", [
-    route("confirm", "routes/ConfirmBooking.tsx"),
+    route("confirm", "routes/ConfirmBooking.tsx", [
+      index("routes/ConfirmBookingForm.tsx"),
+      route("success", "routes/BookingSuccess.tsx"),
+    ]),
   ]),
   ...prefix("auth", [route("confirm", "resources/confirm.ts")]),
 ] satisfies RouteConfig;
