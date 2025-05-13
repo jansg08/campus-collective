d838e82e4e03d15a655e51e56a9c1398ae815a50
Jack Hansjee <jack@hansjee.com>
2025-05-13 14:29:04 +0100

    Refactor the way parameters are injected into square button classes and add support for background image

--- app/components/SquareButton.tsx
+++ app/components/SquareButton.tsx
@@ -3,7 +3,8 @@ import { Link } from "react-router";
 interface SquareButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  colour: "primary" | "secondary";
+  colour: "primary" | "secondary" | "custom";
+  bgUrl?: string;
   size?: "normal" | "smaller" | "small";
   position?: "static" | "relative" | "absolute";
   zIndex?: `z-${string}`;
@@ -26,6 +27,7 @@ interface NonLinkProps extends SquareButtonProps {
 const SquareButton = ({
   children,
   colour,
+  bgUrl,
   isLink = false,
   path,
   size = "normal",
@@ -37,21 +39,42 @@ const SquareButton = ({
   right,
   onClick,
 }: LinkProps | NonLinkProps) => {
-  const classes = `bg-${colour} ${
-    size === "normal" ? "p-1.5 rounded-lg" : "rounded-sm"
-  } ${
-    size === "smaller" && "p-1"
+  const sizeClasses = {
+    withIcon: {
+      normal: "p-1.5 rounded-lg",
+      smaller: "p-1 rounded-sm",
+      small: "rounded-sm",
+    },
+    withBackground: {
+      normal: "size-10 rounded-lg",
+      smaller: "size-7 rounded-sm",
+      small: "size-6 rounded-sm",
+    },
+  };
+  const colourClasses = {
+    primary: "bg-primary text-background hover:brightness-110",
+    secondary: "bg-secondary hover:brightness-105",
+    custom: "",
+  };
+  const classes = `${colour !== "custom" ? `bg-${colour}` : ""} ${
+    sizeClasses[bgUrl ? "withBackground" : "withIcon"][size]
   } ${position} ${top} ${left} ${bottom} ${right} ${zIndex} shadow-lg transition-all cursor-pointer ${
-    colour === "primary"
-      ? "text-background hover:brightness-110"
-      : "hover:brightness-105"
+    colourClasses[colour]
   }`;
   return isLink ? (
-    <Link to={path as string} className={classes}>
+    <Link
+      to={path as string}
+      className={classes}
+      style={bgUrl ? { backgroundImage: `url(${bgUrl})` } : undefined}
+    >
       {children}
     </Link>
   ) : (
-    <button className={classes} onClick={onClick}>
+    <button
+      className={classes}
+      onClick={onClick}
+      style={bgUrl ? { backgroundImage: `url(${bgUrl})` } : undefined}
+    >
       {children}
     </button>
   );
