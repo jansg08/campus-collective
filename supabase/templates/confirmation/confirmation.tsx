2e6cbeffb20785e024e55c081653d7d45588f2c3
Jack Hansjee <jack.hansjee@me.com>
2025-05-18 22:15:58 +0200

    Refactor layout of account confirmation email to improve readability in gmail

--- supabase/templates/confirmation/confirmation.tsx
+++ supabase/templates/confirmation/confirmation.tsx
@@ -7,28 +7,50 @@ config({ path: `${process.cwd()}/.env.local` });
 const Confirmation = () => {
   return (
     <EmailTemplate>
-      <main className="flex items-center justify-center w-screen h-screen">
-        <div className="flex flex-col gap-28 w-80 bg-secondary p-8 rounded-2xl shadow-xl">
-          <Img
-            src={`${process.env.S3_STORAGE_URL}/storage/v1/object/public/assets/cc-logos/Logo.png`}
-            width="100%"
-          />
-          <div className="flex flex-col gap-5">
-            <p>Please click the button below to confirm your email address.</p>
+      <table style={{ borderSpacing: "0px 20px" }}>
+        <tr>
+          <td>
+            <Img
+              src={`${process.env.S3_STORAGE_URL}/storage/v1/object/public/assets/cc-logos/Logo.png`}
+              width="100%"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <h2 className="font-bold">
+              You're one step away from joining Campus Collective.
+            </h2>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>
+              Click the button below to confirm your account so you can start
+              exploring everything your uni has to offer!
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <td>
             <a
+              className="w-full block leading-none font-bold transition-all py-3 rounded-lg bg-primary text-background no-underline text-center shadow-lg"
               href="{{ .SiteURL }}/auth/confirm?token_hash={{ .TokenHash }}&type=email&next={{ .RedirectTo }}"
-              className="w-full leading-none font-bold transition-all py-3 rounded-lg bg-primary text-background no-underline flex items-center justify-center shadow-lg"
               target="_blank"
             >
               <span className="w-min text-nowrap">Confirm Email</span>
             </a>
+          </td>
+        </tr>
+        <tr>
+          <td>
             <p className="text-text-dim text-sm">
               If you did not recently sign up for a Campus Collective account,
               please ignore this email.
             </p>
-          </div>
-        </div>
-      </main>
+          </td>
+        </tr>
+      </table>
     </EmailTemplate>
   );
 };
--- supabase/templates/emailTemplate.tsx
+++ supabase/templates/emailTemplate.tsx
@@ -11,7 +11,7 @@ export const EmailTemplate = ({ children }: EmailTemplateProps) => {
       <Head>
         <Font
           fontFamily="Source Serif 4"
-          fallbackFontFamily={"Times New Roman"}
+          fallbackFontFamily={"Georgia"}
           webFont={{
             url: "https://fonts.gstatic.com/s/sourceserif4/v13/vEFI2_tTDB4M7-auWDN0ahZJW1gb8te1Xb7G.woff2",
             format: "woff2",
@@ -20,12 +20,30 @@ export const EmailTemplate = ({ children }: EmailTemplateProps) => {
         />
         <style>
           {
-            "button { border: none; } p { margin: 0 } body { color: #044c3b; background-color: #f7f4e9 }"
+            "button { border: none; } p { margin: 0 } body { color: #044c3b; background-color: #f7f4e9 } h1,h2,h3,h4,h5,h6 { margin: 0px }"
           }
         </style>
       </Head>
       <Tailwind config={tailwindConfig}>
-        <Body>{children}</Body>
+        <Body className="m-0 w-screen box-border">
+          <main className="absolute top-0 left-0 w-full h-full p-5 box-border">
+            <table className="w-full h-full">
+              <tr>
+                <td></td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="max-w-80 mx-auto bg-secondary p-8 rounded-2xl shadow-xl text-center">
+                    {children}
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+              </tr>
+            </table>
+          </main>
+        </Body>
       </Tailwind>
     </Html>
   );
