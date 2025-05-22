30804717384deda56fe9957ee9792b0adb804558
Jack Hansjee <jack@hansjee.com>
2025-05-22 23:03:37 +0100

    Add horizontal padding to header so that it lines up with the main content

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -52,7 +52,7 @@ const Header = ({ authenticated, isStaff, avatarUrl }: HeaderProps) => {
 
   return (
     <header className="w-full py-2.5 px-5 bg-background-light shadow-below flex justify-center fixed z-40">
-      <div className="w-[min(100%,80rem)] flex flex-col gap-4">
+      <div className="w-[min(100%,80rem)] flex flex-col gap-4 px-5">
         <div className="w-full h-15 flex items-center">
           <div className="w-full flex justify-start">
             <SquareButton
