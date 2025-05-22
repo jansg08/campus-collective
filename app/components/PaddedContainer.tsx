833a5ac0190f96443a84732b69bd3bd324845daa
Jack Hansjee <jack@hansjee.com>
2025-05-22 23:04:08 +0100

    Add option to have no padding with padded container

--- app/components/PaddedContainer.tsx
+++ app/components/PaddedContainer.tsx
@@ -1,7 +1,7 @@
 interface PaddedContainerProps {
   children: React.ReactNode;
   flexGap?: `gap-${number}`;
-  padding?: "normal" | "thick";
+  padding?: "normal" | "thick" | "none";
   margin?: string;
   fullPage?: boolean;
   background?: `bg-${string}` | "";
@@ -20,7 +20,7 @@ const PaddedContainer = ({
   return (
     <div
       className={`w-[min(100%,80rem)] ${
-        padding === "normal" ? "p-5" : "p-10"
+        padding === "none" || (padding === "normal" ? "p-5" : "p-10")
       } ${
         fullPage ? "h-[calc(100vh_-_5rem)]" : ""
       } ${margin} ${background} ${shadow} relative flex flex-col items-center justify-center box-border ${flexGap}`}
