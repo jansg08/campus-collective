fed6fe5b7688b16330a3ceba1972d23039c47cdb
Jack Hansjee <jack@hansjee.com>
2025-05-26 00:28:55 +0100

    Change aligment of sign up form do to new university select field

--- app/routes/SignUp.tsx
+++ app/routes/SignUp.tsx
@@ -35,7 +35,7 @@ export const action = async ({ request }: Route.ActionArgs) => {
 const SignUp = ({ actionData }: Route.ComponentProps) => {
   return (
     <PaddedContainer padding="thick" fullPage>
-      <section className="w-full -translate-y-1/8">
+      <section className="w-full -translate-y-1/24">
         <Outlet />
       </section>
     </PaddedContainer>
