55fda4ab28b8eb55dbeae78c1841a54f0d6ffaa3
Jack Hansjee <jack@hansjee.com>
2025-05-26 14:19:23 +0100

    Add action logic to insert preferred uni when creating an account

--- app/routes/SignUp.tsx
+++ app/routes/SignUp.tsx
@@ -4,23 +4,32 @@ import PaddedContainer from "~/components/PaddedContainer";
 import type { Route } from "./+types/SignUp";
 import { getSupabaseClient } from "~/auth/supabase.server";
 
-interface SignUpProps {}
+interface MetadataProps {
+  is_staff: boolean;
+  university?: number;
+}
 
 export const action = async ({ request }: Route.ActionArgs) => {
   const formData = await request.formData();
   const email = String(formData.get("email"));
   const password = String(formData.get("password"));
+  const university = Number(formData.get("university"));
   const { supabase, headers } = getSupabaseClient(request);
+  const metadata: MetadataProps = {
+    is_staff: false,
+  };
 
   // !implement data validation here
+
+  if (university !== -1) {
+    metadata.university = university;
+  }
 
   const { error } = await supabase.auth.signUp({
     email: email,
     password: password,
     options: {
-      data: {
-        is_staff: false,
-      },
+      data: metadata,
       emailRedirectTo: `${process.env.BASE_URL}/log-in`,
     },
   });
@@ -33,6 +42,7 @@ export const action = async ({ request }: Route.ActionArgs) => {
 };
 
 const SignUp = ({ actionData }: Route.ComponentProps) => {
+  console.log(actionData);
   return (
     <PaddedContainer padding="thick" fullPage>
       <section className="w-full -translate-y-1/24">
